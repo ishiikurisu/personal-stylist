cab745ea5d91d12d93bd25f0796febb3632620b1
Cris Silva Jr <ishiikurisu@yahoo.com>
2019-04-07 17:39:27 -0300

    Caching Tumblr results for testing purposes

--- app/tumblr/Tumblr.rb
+++ app/tumblr/Tumblr.rb
@@ -3,11 +3,18 @@ require 'json'
 
 module Tumblr
   def Tumblr.get_posts blog, api
-    url = URI.parse "http://api.tumblr.com/v2/blog/#{blog}/posts?api_key=#{api}"
-    req = Net::HTTP::Get.new(url.to_s)
-    res = Net::HTTP.start(url.host, url.port) do |http|
-      http.request(req)
+    cache = "b/#{blog}.json"
+    if File.exist? cache
+      raw = File.open(cache, 'r') { |fp| fp.read }
+    else
+      url = URI.parse "http://api.tumblr.com/v2/blog/#{blog}/posts?api_key=#{api}"
+      req = Net::HTTP::Get.new(url.to_s)
+      res = Net::HTTP.start(url.host, url.port) do |http|
+        http.request(req)
+      end
+      raw = res.body
+      File.open(cache, 'w') { |fp| fp.write raw }
     end
-    JSON.parse res.body
+    JSON.parse raw
   end
 end
